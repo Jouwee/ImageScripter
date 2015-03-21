40481ebe30ed7fed55901f0db4cc9d92c12b2a14
Jouwee <nicolaspohren@gmail.com>
2015-03-21 09:52:08 -0300

    Implemented zoom in ImageViewer

--- src/com/jouwee/proto/gui/ImageViewer.java
+++ src/com/jouwee/proto/gui/ImageViewer.java
@@ -8,6 +8,11 @@ package com.jouwee.proto.gui;
 
 import com.jouwee.proto.Image;
 import java.awt.BorderLayout;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
+import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
@@ -21,7 +26,8 @@ import javax.swing.JScrollPane;
  * 
  * @author Jouwee
  */
-public class ImageViewer extends JComponent implements PropertyChangeListener {
+public class ImageViewer extends JComponent implements PropertyChangeListener,
+        MouseWheelListener {
     
     /** Property name - Image */
     public static final String PROP_IMAGE = "PROP_IMAGE";
@@ -31,16 +37,21 @@ public class ImageViewer extends JComponent implements PropertyChangeListener {
     private JLabel imageRendering;
     /** Image currently being shown */
     private Image image;
+    /** Zoom factor */
+    private float zoomFactor;
     /** Property Change Support */
     private final transient PropertyChangeSupport propertyChangeSupport;
     
     /**
      * New image viewer
      */
+    @SuppressWarnings("LeakingThisInConstructor")
     public ImageViewer() {
         super();
         propertyChangeSupport = new java.beans.PropertyChangeSupport(this);
         propertyChangeSupport.addPropertyChangeListener(this);
+        // Default zoom
+        zoomFactor = 1.0f;
         // Initializes the graphics user interface
         initGui();
     }
@@ -61,6 +72,9 @@ public class ImageViewer extends JComponent implements PropertyChangeListener {
      */
     private void setupImageRendering() {
         imageRendering = new JLabel();
+        imageRendering.setHorizontalAlignment(JLabel.CENTER);
+        // Zoom handler
+        imageRendering.addMouseWheelListener(this);
         updateImageRendering();
     }
 
@@ -78,7 +92,26 @@ public class ImageViewer extends JComponent implements PropertyChangeListener {
         if (imageRendering == null || image == null) {
             return;
         }
-        imageRendering.setIcon(new ImageIcon(image.getBufferedImage()));
+        BufferedImage bi = getZoomedImage(image);
+        imageRendering.setIcon(new ImageIcon(bi));
+    }
+    
+    /**
+     * Retorna a imagem com zoom aplicado
+     * 
+     * @param image
+     * @return BufferedImage
+     */
+    private BufferedImage getZoomedImage(Image image) {
+        BufferedImage bi = image.getBufferedImage();
+        // New image size
+        int width = (int) (bi.getWidth() * zoomFactor);
+        int height = (int) (bi.getHeight() * zoomFactor);
+        // Transforms
+        AffineTransform scaleTransform = AffineTransform.getScaleInstance(zoomFactor, zoomFactor);
+        AffineTransformOp bilinearScaleOp = new AffineTransformOp(scaleTransform, AffineTransformOp.TYPE_BILINEAR);
+        // Resize the image
+        return bilinearScaleOp.filter(bi, new BufferedImage(width, height, bi.getType()));
     }
 
     /**
@@ -104,6 +137,18 @@ public class ImageViewer extends JComponent implements PropertyChangeListener {
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         updateImageRendering();
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        if (e.isControlDown()) {
+            zoomFactor -= e.getUnitsToScroll() * 0.005;
+            // Lower limit
+            if (zoomFactor < 0.05) {
+                zoomFactor = 0.05f;
+            }
+            updateImageRendering();
+        }
     }
     
 }
